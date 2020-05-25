b34a9cc92842c8efd833a5dcc18d1cfa527b3f1b
James Wu <ywu@lcs.on.ca>
2020-05-26 00:36:23 +0800

    Created the board and girds of the Game of Life, ready to add rules

--- Animation/Sketch.swift
+++ Animation/Sketch.swift
@@ -7,8 +7,14 @@ class Sketch : NSObject {
     //       Therefore, the line immediately below must always be present.
     let canvas : Canvas
     
-    // Tortoise to draw with
-    let turtle : Tortoise
+    // Define a row on the board
+    var row = Array(repeating: false, count: 50)
+    
+    // Define an empty board
+    var board: [[Bool]] = []
+    
+    // Define cell width and height
+    let size = 10
         
     // This function runs once
     override init() {
@@ -16,26 +22,47 @@ class Sketch : NSObject {
         // Create canvas object – specify size
         canvas = Canvas(width: 500, height: 500)
         
-        // Create turtle to draw with
-        turtle = Tortoise(drawingUpon: canvas)
+        // Iterate through the rows
+        for _ in 1...50 {
+            board.append(row)
+        }
         
-        // Move to middle of screen
-        turtle.penUp()
-        turtle.setPosition(to: Point(x: 250, y: 250))
-        turtle.penDown()
+        print("Done creating board.")
+        
+        // Modify at a specific position
+        //  NOTE: Zero-based
+        //
+        //  row is 0, column is 4
+        board[0][4] = true
 
     }
     
     // This function runs repeatedly, forever, to create the animated effect
     func draw() {
         
-        // Required to bring canvas into same orientation and origin position as last run of draw() function
-        turtle.restoreStateOnCanvas()
-
-        // Move the turtle forward and turn it a bit
-        turtle.forward(steps: 5)
-        turtle.right(by: 1)
+        // Iterate over all the rows and columns
+        // Draw a filled black square when a value is true
+        for row in 0...board.count - 1 {
+            for column in 0...board[row].count - 1 {
+                
+                print(board[row][column])
+                
+                if board[row][column] == true {
+                    
+                    canvas.drawShapesWithFill = true
+                    
+                } else {
+                    
+                    canvas.drawShapesWithFill = false
+                    
+                }
+                
+                canvas.drawRectangle(at: Point(x: column * size, y: row * size), width: size, height: size)
+                
+            }
+        }
         
     }
     
 }
+
