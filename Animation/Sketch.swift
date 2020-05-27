6335e0355cf141399d4c172524b7ab41683a5e26
James Wu <ywu@lcs.on.ca>
2020-05-28 00:26:19 +0800

    Fixed a couple of problems and need t find out why the drawing function isn't working

--- Animation/Sketch.swift
+++ Animation/Sketch.swift
@@ -8,29 +8,14 @@ class Sketch : NSObject {
     let canvas : Canvas
     
     // Define a row on the board
-    var row = Array(repeating: false, count: 50)
+    var row = Array(repeating: false, count: 5)
     
     // Define an empty board
     var board: [[Bool]] = []
     
     // Define cell width and height
-    let size = 10
+    let size = 100
     
-    // Introduce a function that checks how many living and dead cells are around a cell
-    func numberOfCellsAliveAround(row:Int, column:Int)->Int{
-        var numberAlive = 0
-        // Need to find a way when index is out of range?
-        for x in row-1...row+1 {
-            for y in column-1...column+1{
-                if board[x][y] == true {
-                    numberAlive += 1
-                }else if board[row][column] == true {
-                    numberAlive -= 1
-                }
-            }
-        }
-        return numberAlive
-    }
     
     // This function runs once
     override init() {
@@ -39,7 +24,7 @@ class Sketch : NSObject {
         canvas = Canvas(width: 500, height: 500)
         
         // Iterate through the rows
-        for _ in 1...50 {
+        for _ in 1...5 {
             board.append(row)
         }
         
@@ -51,12 +36,12 @@ class Sketch : NSObject {
         //  row is 0, column is 4
         //        board[0][4] = true
         // Test1
-        board[2][3] = true
-        board[2][4] = true
-        board[2][5] = true
-        board[3][4] = true
-        board[3][5] = true
-        board[3][6] = true
+        board[2][2] = true
+        //        board[2][4] = true
+        //        board[2][5] = true
+        //        board[3][4] = true
+        //        board[3][5] = true
+        //        board[3][6] = true
         
     }
     //
@@ -88,32 +73,51 @@ class Sketch : NSObject {
         for row in 1...board.count - 2 {
             for column in 1...board[row].count - 2 {
                 
+                let liveCellCount = numberOfCellsAliveAround(row: row, column: column)
                 //if living cells around a cell is less than 2, the cell dies
-                if numberOfCellsAliveAround(row: row, column: column) < 2  {
+                if liveCellCount < 2  {
                     board[row][column] = false
                 }
                 //                print(numberOfCellsAliveAround(row: row, column: column))
                 //print the number of living cells around a cell if it's not zero to find why the animation isn't working endlessly
-                if numberOfCellsAliveAround(row: row, column: column) != 0{
-                    print(board[row][column])
-                    print(numberOfCellsAliveAround(row: row, column: column))
-                }
+                //                if numberOfCellsAliveAround(row: row, column: column) != 0 && board[row][column] == true{
+                //                    print(board[row][column])
+                //                    print(numberOfCellsAliveAround(row: row, column: column))
                 //if living cells around a cell is greater than 3, the cell dies
-                if numberOfCellsAliveAround(row: row, column: column) > 3  {
+                if liveCellCount > 3  {
                     board[row][column] = false
                 }
                 //if living cells around a cell is exactly 3, the cell resurge
-                if numberOfCellsAliveAround(row: row, column: column) == 3  {
+                if liveCellCount == 3  {
                     board[row][column] = true
                 }
-                //                print("new  \(board[row][column])")
-                
-                
-                
-                
+            }
+            
+            //                print("new  \(board[row][column])")
+            
+            
+            
+            
+        }
+    }
+    // Introduce a function that checks how many living and dead cells are around a cell
+    func numberOfCellsAliveAround(row:Int, column:Int)->Int{
+        var numberAlive = 0
+        // Need to find a way when index is out of range?
+        for x in row-1...row+1 {
+            for y in column-1...column+1{
+                if board[x][y] == true && x != row && y != column {
+                    numberAlive += 1
+                }
+                //                   if board[row][column] == true {
+                //                       numberAlive -= 1
+                //                   }
             }
         }
-        
+        return numberAlive
     }
+
 }
+
+
 
